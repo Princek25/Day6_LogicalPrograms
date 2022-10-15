4981fcde5bfe94ed375c0be308c5f838b25bbe88
Prince <princek2530@gmail.com>
2022-10-15 09:26:29 +0530

    Added: Swap Nibbles

--- src/com/bridgelabz/junitprograms/SwapNibbles.java
+++ src/com/bridgelabz/junitprograms/SwapNibbles.java
@@ -1,2 +1,25 @@
-package com.bridgelabz.junitprograms;public class SwapNibbles {
+package com.bridgelabz.junitprograms;
+
+import java.util.Arrays;
+
+public class SwapNibbles {
+    static int[] swapNibbles(int[] arr) {
+
+        // swap nibbles at first and last of the array
+        // j is used for saving last 4 index of the array
+        int temp, j = arr.length - 4;
+        for (int i = 0; i < 4; i++) { // loop runs 4 times and swap first four element to last four elements
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int [] arr2 = {1,0,1,1,0,0,0,1};
+        int [] arr3 = swapNibbles(arr2);
+        System.out.print(Arrays.toString(arr3));
+    }
 }
